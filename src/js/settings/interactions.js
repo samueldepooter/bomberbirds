e409e11405d9e9b8745ec350216750cd6020abcb
Samuel De Pooter <samueldepooter@gmail.com>
2017-04-03 10:09:58 +0200

    small change in interactions file

---
+++ src/js/settings/interactions.js
@@ -0,0 +1,25 @@
+export default {
+  levers: [
+    {
+      text: `Pull the lever`,
+      hold: false,
+      key: `f`,
+      description: `A lever...`,
+      action: `Blow up shit`
+    },
+    {
+      text: `Pull the lever`,
+      hold: false,
+      key: `f`,
+      description: `A lever...`,
+      action: `Lower shit`
+    },
+    {
+      text: `Pull the lever`,
+      hold: true,
+      key: `f`,
+      description: `A lever...`,
+      action: `Do some mooorrreeeeee`
+    }
+  ]
+};
